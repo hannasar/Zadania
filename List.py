ea359be62c4ca13817aa4913a1730642cf38bde2
Hanna Sarnecka <46069679+hannasar@users.noreply.github.com>
2022-04-25 20:56:54 +0200

    Update List.py
    Function without for loop :)

--- List.py
+++ List.py
@@ -3,9 +3,4 @@ def NewList():
     '''
     Function generates a list of numbers from 2 to 5.5 with a 0.5 step.
     '''
-    List=[]
-    k=2
-    for i in range(8):
-        List.append(k)
-        k+=0.5
-    return List 
+    return list(np.arange(2.0,5.5, 0.5))
